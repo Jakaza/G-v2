d322f50a27b175eb46e039762d058cfec41ae51e
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-06-06 17:00:59 +0200

    Update SkillSet.jsx

--- frontend/src/Components/Skill/SkillSet.jsx
+++ frontend/src/Components/Skill/SkillSet.jsx
@@ -30,13 +30,33 @@ const SkillSet = () => {
             icon: "linux-original",
             level: 98,
         },
+        {
+            name: "html5",
+            icon: "html5-original",
+            level: 98,
+        },
+        {
+            name: "css3",
+            icon: "css3-original",
+            level: 98,
+        },
+        {
+            name: "mongodb",
+            icon: "mongodb-original-wordmark",
+            level: 98,
+        },
+        {
+            name: "amazonwebservices",
+            icon: "amazonwebservices-original-wordmark",
+            level: 98,
+        },
     ]
   return (
     <div className='skillSet'>
     <div className="inner">
      <div className="heading">
         <h1>My Skills</h1>
-        <p>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
+        <p>Here you will find my current array of skills mostly in terms of programming and technology</p>
      </div>
 
      <div className="mainSkillsContainer">
@@ -50,23 +70,7 @@ const SkillSet = () => {
      }
 </ul>
 
-<div className="skills__word-clouds">
-<div className="parallax-inner">
-        <p className='s1'>HTML5</p>
-    <p className='s2'>HTML5</p>
-        <p className='s3'>WebGL</p>
-        <p className='s4'>CSS3</p>
-        <p className='s5'>figma</p>
-        <p className='s6'>antd</p>
-        <p className='s7'>MongoDB</p>
-        <p className='s8'>Gatsby</p>
-        <p className='s9'>ES6</p>
-        <p style="top:120px;left:22%;font-size:12px">blender</p>
-
-
-</div>
-</div>
-
+  
      </div>
 
 
