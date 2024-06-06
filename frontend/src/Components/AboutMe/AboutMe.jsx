e384ff09ef5951f186d5773b4dc849745d847517
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-06-06 17:00:56 +0200

    Update AboutMe.jsx

--- frontend/src/Components/AboutMe/AboutMe.jsx
+++ frontend/src/Components/AboutMe/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
         <div className="inner">
          <div className="heading">
             <h1>About Me</h1>
-            <p>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
+            <p>Here you will find more information about me, what I do, and my experience mostly in terms of programming and technology</p>
          </div>
          <AboutMeContent/>
         </div>
