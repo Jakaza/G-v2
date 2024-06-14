8b5300bf08bc28c8ce0cc55451218d12942b5c7c
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-06-14 12:04:00 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import AboutMe from './Components/AboutMe/AboutMe'
-import SkillSet from './Components/Skill/SkillSet'
+import { useState } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import AboutMe from "./Components/AboutMe/AboutMe";
+import SkillSet from "./Components/Skill/SkillSet";
+import Projects from "./Components/Project/Projects";
+import Contact from "./Components/Contact/Contact";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Hero/>
-    <AboutMe/>
-    <SkillSet/>
+      <Navbar />
+      <Hero />
+      <AboutMe />
+      <SkillSet />
+      <Projects />
+      <Contact />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
