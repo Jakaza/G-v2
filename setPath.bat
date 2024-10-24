27ce70aaa03799a23a0e9dc7b87315b45ead586c
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-10-24 10:53:22 +0200

    Update setPath.bat
    Set Path For Nodejs

--- setPath.bat
+++ setPath.bat
@@ -1,3 +1,3 @@
-@cd C:\Users\User_Lab\Documents\GitHub\G-v2
-@set PATH=C:\Users\User_Lab\Documents\node-v20.15.0-win-x64;%PATH%
+@cd C:\Users\STUDENT\Documents\GitHub\G-v2
+@set PATH=C:\Users\STUDENT\Documents\node-v20.18.0-win-x64;%PATH%
 @cmd.exe /K
