64e665f58fc9ab773d7f41b1bbe1b6c4749cdb8d
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-10-24 10:53:27 +0200

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+const express = require("express");
+const initDb = require('./config/dbConnection');
+const authRouter = require("./routes/auth");
+
+var app = express()
+initDb();
+
+app.use("/api/auth", authRouter)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT , ()=>{
+    console.log(`Server is running at ${PORT}`);
+})
